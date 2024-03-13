35751a9ab5f81dcfe7a80c0d53acbee527eadce9
lluisalinhares <lluisalinhares@gmail.com>
2024-03-13 13:29:51 -0300

    Inserindo nomes e idades/pesquisando nomes e idades

---
+++ Exercícios/ex011.py
@@ -0,0 +1,31 @@
+#Armazenando nomes e idades.
+
+nomes = []
+idades = []
+
+while True:
+    nome = input("Insira um nome. Para encerrar, digite fim ")
+    if nome != 'fim':
+        nomes.append(nome)
+        idades.append(int(input("Insira a idade: ")))
+    else:
+        break
+
+for i in range(len(nomes)):
+    print(nomes[i], 'tem', idades[i], 'anos')
+
+
+#Nesta segunda parte do script, vamos implementar uma busca nos nomes e idades.
+
+pesquisa = input("Insira um nome ou idade para pesquisar: ")
+
+if pesquisa in nomes:
+    for i in range(len(nomes)):
+        if nomes[i] == pesquisa:
+            print(idades[i])
+elif pesquisa.isnumeric() and int(pesquisa) in idades:
+    for i in range(len(idades)):
+        if idades[i] == int(pesquisa):
+            print(nomes[i])
+else:
+    print("Nenhum resultado encontrado")
