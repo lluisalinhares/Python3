56e01df83c797fffb010f9448839367d96d555e5
lluisalinhares <lluisalinhares@gmail.com>
2024-03-13 11:45:12 -0300

    For/in range

---
+++ Exercícios/ex010.py
@@ -0,0 +1,17 @@
+# O comando in range() repete uma variável o tanto de vezes que for especificado.
+
+for i in range(5):
+    print(i)
+
+# O comando "for i in palavra:" faz a variável "i" repetir a quantidade de
+# caracteres que contém a variável "palavra".
+
+palavra = 'codigofonte'
+for i in palavra:
+    print(i)
+
+# Listas
+
+lista = [1, 2, 54, 'oi']
+for num in lista:
+    print(num)
